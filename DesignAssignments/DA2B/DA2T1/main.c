624b4344f86c4732a32a6db4ff7be4ec24b7bea8
lees106 <47095245+lees106@users.noreply.github.com>
2019-03-09 18:40:27 -0800

    Add files via upload

---
+++ DesignAssignments/DA2B/DA2T1/main.c
@@ -0,0 +1,18 @@
+/*
+ * DA2T1.c
+ *
+ * Created: 3/9/2019 4:35:45 PM
+ * Author : Steven Lee
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ DesignAssignments/DA2B/DA2T2/main.c
@@ -0,0 +1,22 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+int main(void) {
+	DDRB  |= (1<<5) | (1<<2); // Set PB5 and PB2 as output
+	PORTB |= (1<<5);		  // Set PB5 high to turn off LED
+	PORTD |= 1<<2;			  // Enable Pull Up on PD2
+	
+	EICRA = 0x2;			  // Make INTO falling edge triggered
+	EIMSK = (1<<INT0);		  // Enable external INT0
+	
+	sei();				      //Enables global interrupt
+
+	while (1) {};
+}
+
+ISR (INT0_vect) {			  // ISR for external INT0
+	PORTB ^= (1<<2);		  //Toggle LED
+	_delay_ms(1250);
+}
